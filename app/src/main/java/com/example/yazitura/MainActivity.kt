f996adf9b324d772391e12b897661ad97fbb7b01
ozgurkf-coder <ozgurkf@gmail.com>
2026-05-04 11:15:37 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/yazitura/MainActivity.kt
+++ app/src/main/java/com/example/yazitura/MainActivity.kt
@@ -9,11 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tv = TextView(this)
-        tv.text = "Yazı Tura Oyunu"
-        tv.textSize = 24f
 
-        setContentView(tv)
     }
 }
 ``
